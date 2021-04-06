dc9889595cd54da89254b98d3401b2f300a382f5
Osada97 <osadamanohara55@gmail.com>
2021-04-06 18:15:01 +0530

    decrease stagger duration

--- components/animation.js
+++ components/animation.js
@@ -20,14 +20,14 @@ export const pageAnimation = {
 export const stagger = {
   show: {
     transition: {
-      staggerChildren: 0.5,
+      staggerChildren: 0.2,
     },
   },
 };
 
 export const titleAnim = {
   hidden: { y: 50, opacity: 0 },
-  show: { y: 0, opacity: 1, transition: { duration: 0.8, ease: "easeOut" } },
+  show: { y: 0, opacity: 1, transition: { duration: 0.5, ease: "easeOut" } },
 };
 
 export const buttonAnim = {
