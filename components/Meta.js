ec87825b7e67701de02a845986822893b3cdf3a9
Osada97 <osadamanohara55@gmail.com>
2021-04-16 11:48:22 +0530

    Added meta description

--- components/Meta.js
+++ components/Meta.js
@@ -6,6 +6,10 @@ export default function Meta({ title = "OnDev", keywords, description }) {
     <div>
       <Head>
         <meta charSet="utf-8" />
+        <meta
+          name="description"
+          content="ONdev technologies is a Sri Lankan startup company , which operates projects related to web development,  virtual systems and sustainable applications with ICT , in the fields of business and technology."
+        />
         <link rel="icon" href="/favicon.ico" />
         <title>{title}</title>
       </Head>
